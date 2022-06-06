1ea0b40f65b88b8804d70d90802dad4f73ccb0a4
Esdvska <esdvska@gmail.com>
2022-06-06 20:14:54 +0200

    Added heartbeat and addAdvertismesn request

--- app.js
+++ app.js
@@ -1,73 +1,54 @@
-// npm i dotenv
-// npm i mongoose
-// gdy korzystamy z mongoose nie ma potrzeby instalowania mongo
+// Wykorzystując bazę danych MongoDB, stwórzmy aplikację typu lista zadań (todo list), wykonaną w podejściu REST API. Aplikacja powinna pozwalać na:
+
+// dodanie nowego dokumentu do naszej bazy w postaci:
+// {
+// 	"task": "naprawić samochód",
+// 	"description": "coś stuka po lewej",
+// 	"isCompleted": false,
+// 	"createdTime": "05-05-2022"
+// }
+// zmodyfikowanie zadania - poprzez określenie czy dane zadanie zostało wykonane
+// {
+// 	"isCompleted": true
+// }
+// usunięcie zadania
+// pobranie wszystkich zadań
+// pobranie pojedynczego zadania
+
 require("dotenv").config();
-const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_CONNECTION;
+const express = require("express");
+const status = require("http-status");
 
-// definicja schematu kolecji bazodanowej
-// const TaskSchema = new mongoose.Schema(
-//   {
-//     task: String,
-//     description: String,
-//     completed: {
-//       type: Boolean,
-//       default: false, // wartość domyślna
-//     },
-//     createdTime: {
-//       type: Date,
-//       default: new Date(), // wartość domyślna
-//     },
-//     value: {
-//       type: Number,
-//       // walidacja
-//       min: 10,
-//       max: 100,
-//       required: true,
-//     },
-//   },
-//   { timestamps: true } // automatyczne dodawanie włąściwości z czasem utworzenia i modyfikacji dokumentu w bazie
-// );
+const { main, addNewAdvertisment } = require("./db");
+const app = express();
 
-// middleware/hook wykonywany przed zapisam dokumentu
-// TaskSchema.pre("save", function () {
-//   console.log("task is going to be saved");
-// });
+app.use(express.json());
 
-// middleware/hook wykonywany po zapisie dokumentu
-// TaskSchema.post("save", function () {
-//   console.log("task saved");
-// });
+main()
+  .then(() => {
+    app.get("/heartbeat", (req, res) => res.send(new Date()));
 
-// tworzenie modelu danych dla konkretnej kolekcji na podstawie uprzednio stworzonego schematu
-// const TaskModel = mongoose.model("mongooseTasks", TaskSchema); // pierwszy parametr to nazwa kolekcji w bazie danych
+    app.post("/advertisments", async (req, res) => {
+      const newAdvertisment = req.body;
 
-async function main() {
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+      // warto dodać sprawdzenie czy newAdvertisment posiada odpowiednie właściwości, gdy nie to zwracać kod 400 bez dodawania do bazy
+
+      const result = await addNewAdvertisment(newAdvertisment);
+
+      if (result.insertedCount === 1) {
+        res.statusCode = status.CREATED;
+      } else {
+        res.statusCode = status.INTERNAL_SERVER_ERROR;
+      }
+
+      res.send();
+    });
+  })
+  .finally(() => {
+    app.get("/heartbeat", (req, res) => {
+      res.send(new Date());
+    });
+
+    app.listen(process.env.PORT, () => console.log("server started"));
   });
-
-  //   const newTask = new TaskModel({
-  //     task: "my new mongoose task 2",
-  //     description: "task description 2",
-  //     value: 20,
-  //   });
-
-  //   let result = await newTask.save(); // zapis do bazy nowego dokumentu
-  //   console.log(result);
-
-  //   newTask.completed = true;
-  //   result = await newTask.save(); // aktualizacja wcześniej zapisanego dokumentu
-  //   console.log(result);
-
-  //   const tasks = await TaskModel.find(); // pobranie wszystkich dokumentów z kolekcji
-  //   tasks.forEach((task) => {
-  //     console.table(task);
-  //   });
-
-  //   await TaskModel.deleteMany({ completed: "true" }); // usunięcie z bazy wszystkich dokumentów z właściwością completed = true
-}
-
-main().catch((err) => console.log(err));
